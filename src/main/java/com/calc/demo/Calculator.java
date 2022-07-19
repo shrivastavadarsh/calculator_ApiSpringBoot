8bafb1bf16df480e6c58ba74452b6586813762fc
Adarsh Raj Shrivastava <adarshraj458@gmail.com>
2022-07-19 15:30:17 +0530

    asmd functions added

---
+++ src/main/java/com/calc/demo/Calculator.java
@@ -0,0 +1,13 @@
+package com.calc.demo;
+
+public class Calculator {
+    private  double finalValue ;
+
+    public double getFinalValue() {
+        return this.finalValue;
+    }
+
+    public void setFinalValue(double finalValue) {
+        this.finalValue = finalValue;
+    }
+}
---
+++ src/main/java/com/calc/demo/DataForCalc.java
@@ -0,0 +1,31 @@
+package com.calc.demo;
+
+public class DataForCalc {
+    private double operand1;
+    private double operator;
+    private double operand2;
+
+    public double getOperand1() {
+        return operand1;
+    }
+
+    public void setOperand1(double operand1) {
+        this.operand1 = operand1;
+    }
+
+    public double getOperator() {
+        return operator;
+    }
+
+    public void setOperator(double operator) {
+        this.operator = operator;
+    }
+
+    public double getOperand2() {
+        return operand2;
+    }
+
+    public void setOperand2(double operand2) {
+        this.operand2 = operand2;
+    }
+}
