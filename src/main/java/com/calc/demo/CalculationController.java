15e9afad3a52b0b9f0a9a5e29acfd829e9a17cc3
Adarsh Raj Shrivastava <adarshraj458@gmail.com>
2022-07-20 13:16:39 +0530

    percentage api added

--- src/main/java/com/calc/demo/CalculationController.java
+++ src/main/java/com/calc/demo/CalculationController.java
@@ -7,78 +7,87 @@ import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class CalculationController {
-    private static  final String template = "Calculated string  , %s!" ;
-    private final AtomicLong counter= new AtomicLong();
-    private String [] list;
-
-
+    private static final String template = "Calculated string  , %s!";
+    private final AtomicLong counter = new AtomicLong();
+    private String[] list;
 
 
     @GetMapping("/a")
-    public Calculation add(@RequestParam(value ="operands" ,defaultValue = "world")String[] val){
+    public Calculation add(@RequestParam(value = "operands", defaultValue = "world") String[] val) {
 
 //        System.out.println(name);
-        for(String c:val) System.out.println(c);
-        double ans  = 0;
-        for(String s:val){
-            ans+=(Double.parseDouble(s));
+        for (String c : val) System.out.println(c);
+        double ans = 0;
+        for (String s : val) {
+            ans += (Double.parseDouble(s));
         }
 
-        return new Calculation(String.format(template,ans));
+        return new Calculation(String.format(template, ans));
 
     }
 
 
     @GetMapping("/s")
-    public Calculation subtract(@RequestParam(value ="operands" ,defaultValue = "world")String[] val){
+    public Calculation subtract(@RequestParam(value = "operands", defaultValue = "world") String[] val) {
 
 //        System.out.println(name);
-        for(String c:val) System.out.println(c);
-        double ans  = Double.parseDouble(val[0])-Double.parseDouble(val[1]);
+        for (String c : val) System.out.println(c);
+        double ans = Double.parseDouble(val[0]) - Double.parseDouble(val[1]);
 
 
-        return new Calculation(String.format(template,ans));
+        return new Calculation(String.format(template, ans));
 
     }
+
     @GetMapping("/m")
-    public Calculation multiply(@RequestParam(value ="operands" ,defaultValue = "world")String[] val){
+    public Calculation multiply(@RequestParam(value = "operands", defaultValue = "world") String[] val) {
 
         System.out.println("multiplication triggere");
 //        for(String c:val) System.out.println(c);
-        double ans  = Double.parseDouble(val[0])*Double.parseDouble(val[1]);
+        double ans = Double.parseDouble(val[0]) * Double.parseDouble(val[1]);
 
 
-        return new Calculation(String.format(template,ans));
+        return new Calculation(String.format(template, ans));
 
     }
+
     @GetMapping("/d")
-    public Calculation divide(@RequestParam(value ="operands" ,defaultValue = "world")String[] val){
+    public Calculation divide(@RequestParam(value = "operands", defaultValue = "world") String[] val) {
 
 //        System.out.println(name);
 //        for(String c:val) System.out.println(c);
-        if(val[1].equals(0))
-            return new Calculation(String.format(template,"denominator cannot be 0"));
+        if (val[1].equals(0))
+            return new Calculation(String.format(template, "denominator cannot be 0"));
 
         else {
-            double ans  = Double.parseDouble(val[0])/Double.parseDouble(val[1]);
+            double ans = Double.parseDouble(val[0]) / Double.parseDouble(val[1]);
 
 
-            return new Calculation(String.format(template,ans));
+            return new Calculation(String.format(template, ans));
 
         }
 
 
     }
 
+    @GetMapping("/p")
+    public Calculation percent(@RequestParam(value = "operands", defaultValue = "world") String[] val) {
+
+//        System.out.println(name);
+//        for(String c:val) System.out.println(c);
+        if (val[1].equals(0))
+            return new Calculation(String.format(template, "denominator cannot be 0"));
+
+        else {
+            double ans = Double.parseDouble(val[0]) / Double.parseDouble(val[1]);
+            ans *= 100;
 
 
-    @GetMapping("/hello/{name}")
-    public Calculation testingpathApi(@PathVariable("name")String name){
-        return new Calculation(String.format(template,name));
+            return new Calculation(String.format(template, ans));
+
+        }
+
+
     }
-
-//    public Calculation add(@RequestParam()
-
-
 
 }
